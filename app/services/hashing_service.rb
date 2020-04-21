4ec210db6c9f1b6d8e5791fc24da494f4ac97b26
Anibal Caixinha <anibal.caixinha@compta.tpt>
2020-04-21 19:57:04 +0100

    replaced encription key in hashing_service, by hardcoded version for demonstration

--- app/services/hashing_service.rb
+++ app/services/hashing_service.rb
@@ -4,7 +4,9 @@ class HashingService
   include Singleton
   extend Forwardable
   def initialize
-    @hashids = Hashids.new(Rails.application.credentials.secret_key_base)
+    #@hashids = Hashids.new(Rails.application.credentials.secret_key_base)
+    # the key is hardcoded for demonstration purposes
+    @hashids = Hashids.new '68e3b0eaebfa83338f69d66552221841aa380e4860c4493bf1b6edf2bc73ecc1a5ae43be5c57f27919bf6b7a3e102bd6a2c2c6b6b77e9fccb31c88be0716b638'
   end
 
   def_delegators :@hashids, :encode, :decode
